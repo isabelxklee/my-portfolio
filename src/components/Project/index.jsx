ddb08318e0699d6cf8420ac020d870c34af28b27
isabelxklee <hi@kleetime.com>
2021-09-08 16:13:13 -0400

    created Project component

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Project = ({ project }) => {
+  return <p>{project.title}</p>;
+};
+
+export default Project;
--- src/pages/Work/index.jsx
+++ src/pages/Work/index.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, H2 } from "../../styles";
+import sanityClient from "../../client.js";
+import Project from "../../components/Project";
 
 const Work = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
+          _id,
+          title,
+          "slug": slug.current,
+          "tags": tags[]->title,	
+          mainImage{
+            asset->{
+            url
+          }
+        }
+      }`
+      )
+      .then((data) => setProjects(data))
+      .catch(console.error);
+  }, []);
+
   return (
     <>
       <Box>
         <H2>work</H2>
+        {projects &&
+          projects.map((project) => (
+            <Project project={project} key={project._id} />
+          ))}
       </Box>
     </>
   );
