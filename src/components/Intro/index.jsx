5ee88d5694829674c9166e19af82d0e8468cf165
isabelxklee <hi@kleetime.com>
2021-09-08 14:31:11 -0400

    added styles for intro

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { IntroContainer } from "./styles";
+import { P } from "../../styles";
 
 const Intro = () => (
   <IntroContainer>
-    <p>internet-surfing, code-wrangling, non-binary software developer.</p>
-    <p>BACKGROUND IN product design, EDUCATION, AND KICKING A**.</p>
+    <P>internet-surfing, code-wrangling, non-binary software developer.</P>
+    <P>BACKGROUND IN product design, EDUCATION, AND KICKING A**.</P>
   </IntroContainer>
 );
 
--- src/styles.js
+++ src/styles.js
@@ -5,4 +5,17 @@ export const StyledLink = styled(Link)`
   color: #000;
   text-decoration: none;
   font-size: 28px;
+
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
+`;
+
+export const P = styled.p`
+  font-size: 28px;
+  margin: 0;
+
+  @media (max-width: 600px) {
+    font-size: 24px;
+  }
 `;
