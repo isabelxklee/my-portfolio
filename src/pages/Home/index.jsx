46a9cd92825515b5f0309bef51e6d5b7532ea446
Isabel Lee <isabelxklee@gmail.com>
2022-01-27 00:21:30 -0500

    created doodle component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,46 +1,48 @@
-import React, {useState, useEffect} from 'react'
-import {H1} from '../../styles'
-import {Title, TitleImage} from './styles'
-import Intro from '../../components/Intro'
+import React from 'react' // {useState, useEffect}
+import {Title, TitleImage, DoodleComputer, DoodleGlasses, DoodleHat} from './styles'
 // import Work from '../Work'
 import About from '../About'
 import Contact from '../Contact'
 import Footer from '../../components/Footer'
-import sanityClient from '../../client.js'
+// import sanityClient from '../../client.js'
 import nameImage from '../../assets/name-image.png'
+import doodleComputer from '../../assets/doodle_computer.png'
+import doodleGlasses from '../../assets/doodle_glasses.png'
+import doodleHat from '../../assets/doodle_hat.png'
 
 const Home = () => {
-  const [images, setImages] = useState([])
+  // const [images, setImages] = useState([])
 
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == "imageAsset"] {
-            _id,
-            altText,
-            title,
-            "category": category[0]->title,
-            image{
-            asset->{
-              url
-            }
-          }
-        }`
-      )
-      .then((data) => setImages(data))
-      .catch(console.error)
-  }, [])
+  // useEffect(() => {
+  //   sanityClient
+  //     .fetch(
+  //       `*[_type == "imageAsset"] {
+  //           _id,
+  //           altText,
+  //           title,
+  //           "category": category[0]->title,
+  //           image{
+  //           asset->{
+  //             url
+  //           }
+  //         }
+  //       }`
+  //     )
+  //     .then((data) => setImages(data))
+  //     .catch(console.error)
+  // }, [])
 
-  const filterImages = (string) => {
-    return images.filter((image) => image.category === string)
-  }
-
-  console.log(images)
+  // const filterImages = (string) => {
+  //   return images.filter((image) => image.category === string)
+  // }
 
   return (
     <>
       <Title>
         <h1 style={{opacity: 0, margin: 0}}>Isabel K. Lee</h1>
+        <DoodleComputer src={doodleComputer} alt="" />
+        <DoodleGlasses src={doodleGlasses} alt="" />
+        <DoodleHat src={doodleHat} alt="" />
         <TitleImage src={nameImage} alt="Isabel K. Lee" />
       </Title>
       {/* <Intro images={filterImages('Intro')} /> */}
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -9,7 +9,25 @@ export const TitleImage = styled.img`
   width: 50%;
   display: block;
   position: absolute;
-  top: 30%;
+  top: 35%;
   left: 50%;
   transform: translate(-50%, -50%);
+`
+
+export const DoodleComputer = styled.img`
+  display: block;
+  position: absolute;
+  width: 175px;
+  left: 5%;
+`
+
+export const DoodleGlasses = styled(DoodleComputer)`
+  left: unset;
+  right: 5%;
+  top: 15%;
+`
+
+export const DoodleHat = styled(DoodleComputer)`
+  top: 55%;
+  left: 10%;
 `
