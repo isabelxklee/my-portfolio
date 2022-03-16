148a2b706a5e3c66935dc3c9c16037017581ec9f
isabelxklee <hi@kleetime.com>
2022-03-15 21:39:07 -0400

    installed react and added HTML elements to press component

--- src/pages/Press/index.jsx
+++ src/pages/Press/index.jsx
@@ -1,11 +1,41 @@
 import React, {useState} from 'react'
+import ToggleSection from '../../components/ToggleSection'
 
 const Press = () => {
   const [pressItems, setPressItems] = useState(null)
+  const [display, setDisplay] = useState(false)
 
-  console.log(pressItems)
+  const handleClick = () => {
+    setDisplay((display) => !display)
+  }
 
-  return <h1>hello world</h1>
+  console.log(pressItems, setPressItems())
+
+  return (
+    <>
+      <ToggleSection display={display} handleClick={handleClick} title={'press'} />
+      {/* {pressItems && (
+        <Box>
+          <Half>
+            <SpaceP>
+              Hello, fellow Internet surfer! Thank you for visiting my corner of the web. 🌞
+            </SpaceP>
+            <SpaceP>
+              My name is Isabel K. Lee and I’m a full-stack developer. I go by they/them pronouns
+              and have a background in design and product strategy. I really enjoy working on design
+              systems, web applications, and early- to mid-stage startups.
+            </SpaceP>
+            <SpaceP>
+              Before I started my tech career, I studied Middle East foreign policy and philosophy
+              in Montreal. I care deeply about political activism, cooking for others, and taking
+              care of my large, adult son, Finklestein (he's actually a cat).
+            </SpaceP>
+            <SpaceP>Let’s build something together!</SpaceP>
+          </Half>
+        </Box>
+      )} */}
+    </>
+  )
 }
 
 export default Press
