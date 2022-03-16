c1463c60d0872d14261dc0f70365725fbc22e384
isabelxklee <hi@kleetime.com>
2022-03-15 21:51:39 -0400

    deployed sanity changes

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,7 +5,6 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import image from './image'
@@ -30,8 +29,5 @@ export default createSchema({
     project,
     link,
     press,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
   ]),
 })
