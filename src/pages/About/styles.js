65d90bf4f3da2a9eff91611dbd42076e0d665914
isabelxklee <hi@kleetime.com>
2022-03-16 00:31:48 -0400

    added image to about section

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -3,4 +3,17 @@ import styled from 'styled-components'
 export const AboutContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+
+  p {
+    font-size: 24px;
+  }
+`
+
+export const ImageContainer = styled.div`
+  display: block;
+
+  img {
+    width: 100%;
+  }
 `
