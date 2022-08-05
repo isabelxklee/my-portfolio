b0ab4593e9ac42316cfa43f28a600ffceac3b595
isabelxklee <hi@kleetime.com>
2022-08-05 14:12:53 -0400

    updated tech writing

--- src/pages/TechWriting/index.jsx
+++ src/pages/TechWriting/index.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import sanityClient from '../../client.js'
 // import Project from '../../components/Project'
 import {ProjectContainer} from '../Work/styles'
+import {PressItem} from '../Press/styles'
+import * as Styled from '../../styles'
 
 const TechWriting = () => {
   const [techWritingPosts, setTechWritingPosts] = useState(null)
@@ -13,6 +15,7 @@ const TechWriting = () => {
           _id,
           title,
           description,
+          url,
           mainImage{
             asset->{
             url
@@ -28,9 +31,14 @@ const TechWriting = () => {
     <>
       {techWritingPosts && (
         <ProjectContainer>
-          {/* techWritingPosts.map((project) => <Project project={project} key={project._id} />) */}
           {techWritingPosts.map((post) => (
-            <div key={post._id}>{post.title}</div>
+            <PressItem key={post._id}>
+              <Styled.H3>
+                <Styled.ExternalLink href={post.url} target="_blank" rel="nolink_referrer">
+                  {post.title} 🔗
+                </Styled.ExternalLink>
+              </Styled.H3>
+            </PressItem>
           ))}
         </ProjectContainer>
       )}
