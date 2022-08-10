5bf500d1f816f929ee15c69b25fc2e9cf6ab9421
isabelxklee <hi@kleetime.com>
2022-08-10 11:35:16 -0400

    refactored footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,43 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {SmallP, ExternalLink} from '../../styles'
 import {FooterContainer} from './styles'
-import sanityClient from '../../client.js'
 
 const Footer = () => {
-  const [links, setLinks] = useState(null)
-
-  useEffect(() => {
-    sanityClient
-      .fetch(
-        `*[_type == "link" && category[0]->title == "Footer"] {
-            _id,
-            title,
-            url,
-            "category": category[0]->title,
-        }`
-      )
-      .then((data) => setLinks(data))
-      .catch(console.error)
-  }, [])
-
   const copyright = 'Copyright © 2022 Isabel K. Lee. All rights reserved.'
 
   return (
     <FooterContainer>
       <SmallP>{copyright.toUpperCase()}</SmallP>
-      {links && (
-        <SmallP>
-          BUILT WITH{' '}
-          <ExternalLink href={links[0].url} rel="nolink_referrer" target="_blank">
-            {links[0].title.toUpperCase()}
-          </ExternalLink>{' '}
-          AND{' '}
-          <ExternalLink href={links[1].url} rel="nolink_referrer" target="_blank">
-            {links[1].title.toUpperCase()}
-          </ExternalLink>
-          .
-        </SmallP>
-      )}
+      <SmallP>
+        BUILT WITH{' '}
+        <ExternalLink
+          href={'https://www.sanity.io/docs/getting-started'}
+          rel="nolink_referrer"
+          target="_blank"
+        >
+          Sanity.io
+        </ExternalLink>{' '}
+        AND{' '}
+        <ExternalLink
+          href={'https://reactjs.org/docs/getting-started.html'}
+          rel="nolink_referrer"
+          target="_blank"
+        >
+          React.js
+        </ExternalLink>
+        .
+      </SmallP>
     </FooterContainer>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,4 +2,8 @@ import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
   margin-top: 80px;
+
+  @media (max-width: 960px) {
+    margin: 80px 0;
+  }
 `
