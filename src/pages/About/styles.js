ab8068593dc6832272eb7eb2a5abe95670352951
isabelxklee <hi@kleetime.com>
2022-08-10 00:32:29 -0400

    updated styles for about component

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,16 +1,14 @@
 import styled from 'styled-components'
 
 export const AboutContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 16px;
+  width: 65%;
 
   p {
     font-size: 24px;
   }
 
-  @media (max-width: 800px) {
-    grid-template-columns: 1fr;
+  @media (max-width: 960px) {
+    width: 100%;
   }
 `
 
