de912601d5afdb03c3b2b8db5dc213d8e912e379
isabelxklee <hi@kleetime.com>
2022-08-09 23:46:19 -0400

    created index CSS file and refactored intro images

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import * as Styled from '../../styles'
-import {IntroImage, ImageContainer} from './styles'
+// import {IntroImage, ImageContainer} from './styles'
 import sanityClient from '../../client.js'
 
 const Intro = ({description}) => {
@@ -20,32 +20,40 @@ const Intro = ({description}) => {
           }
         }`
       )
-      .then((data) => setImages(data))
+      .then((data) => {
+        let filteredImages = data.filter((image) => image.category === 'Intro')
+        setImages(filteredImages)
+      })
       .catch(console.error)
   }, [])
 
-  const thisCategory = 'Intro'
+  const generateId = (element) => `id-${images.indexOf(element)}`
+
+  console.log(images)
 
   return (
     <>
+      <Styled.P $position="relative" $zIndex={1}>
+        {description.toUpperCase()}
+      </Styled.P>
+      {/* <ImageContainer> */}
+      {images &&
+        images.map((image) => {
+          return (
+            <img
+              key={image._id}
+              id={generateId(image)}
+              src={image.image.asset.url}
+              alt={image.altText}
+            />
+          )
+        })}
+      {/* </ImageContainer> */}
+
       <Styled.SiteTitle>
         <Styled.H1>ISABEL K. LEE</Styled.H1>
         <Styled.H1>2022.</Styled.H1>
       </Styled.SiteTitle>
-
-      <Styled.Box>
-        <Styled.P $position="relative" $zIndex={1}>
-          {description.toUpperCase()}
-        </Styled.P>
-        <ImageContainer>
-          {images &&
-            images
-              .filter((image) => image.category === thisCategory)
-              .map((image) => (
-                <IntroImage key={image._id} src={image.image.asset.url} alt={image.altText} />
-              ))}
-        </ImageContainer>
-      </Styled.Box>
     </>
   )
 }
